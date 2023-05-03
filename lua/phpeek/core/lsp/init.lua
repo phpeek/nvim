b1aebd31409e435c90b8960266dc1a132ae201c5
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2023-05-03 19:02:31 +0200

    removes unsued config and moves everyting out of core

--- lua/phpeek/core/lsp/init.lua
+++
@@ -1,1 +0,0 @@
-require("phpeek.core.lsp.mason")
--- lua/phpeek/init.lua
+++ lua/phpeek/init.lua
@@ -2,9 +2,8 @@
 -- phpeek configuration initialization script
 --------------------------------------------------------------------------------
 
-require("phpeek.packer")
--- runs plugins first so that everything is available for later stages
-require('phpeek.core.options')
-require('phpeek.core.keymaps')
-require('phpeek.core.autocmd')
+require('phpeek.wsl-clipboard')
+require('phpeek.packer')
+require('phpeek.remap')
+require('phpeek.options')
 
